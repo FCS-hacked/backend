c4973859973e5905abecb678e31b10dbcc19c401
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-10-31 00:33:15 +0530

    minor

--- documents/models.py
+++ documents/models.py
@@ -7,7 +7,7 @@ class Document(models.Model):
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="documents")
-    shared_with = models.ManyToManyField(CustomUser, related_name="shared_documents")
+    shared_with = models.ManyToManyField(CustomUser, related_name="shared_documents", blank=True)
     sha_256 = models.CharField(max_length=64, null=True, blank=True)
 
     def is_signed_by(self, custom_user):
