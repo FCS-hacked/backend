3198ef6226311c515f79f386626a93bb1a649b4e
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-11-25 23:26:54 +0530

    media URL

--- backend/urls.py
+++ backend/urls.py
@@ -21,11 +21,12 @@ from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 from backend import settings
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('authentication/', include('authentication.urls')),
-                  path('documents/', include('documents.urls')),
-                  path('unauth/', include('unauth.urls')),
-                  path('products/', include('products.urls')),
-                  path('openapi/', SpectacularAPIView.as_view(), name='schema'),
-                  path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('authentication/', include('authentication.urls')),
+    path('documents/', include('documents.urls')),
+    path('unauth/', include('unauth.urls')),
+    path('products/', include('products.urls')),
+    path('openapi/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/migrations/0005_order_prescription_alter_order_invoice.py
+++ products/migrations/0005_order_prescription_alter_order_invoice.py
@@ -5,6 +5,7 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+
     dependencies = [
         ('documents', '0004_document_signed_by_hospital_and_more'),
         ('products', '0004_alter_order_invoice_alter_orderitem_order'),
