f92380a26b1043ac44b26be9bd1f324ab7a0c48c
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-11-25 13:48:56 +0530

    Products addition

--- products/urls.py
+++ products/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter() if settings.DEBUG else routers.SimpleRouter()
 urlpatterns = [
     path('self/products/', ProductListCreateAPIView.as_view(), name='products-self'),
     path('products/<int:pharmacy_id>', list_pharmacy_products, name='products'),
-    path('patients/create_order', create_order, name='create_order'),
+    path('patients/create_order/', create_order, name='create_order'),
 ]
 
 urlpatterns += router.urls
