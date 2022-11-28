742a059bd0e769cbbf80dd270ffd9f498621ba43
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-11-28 21:52:32 +0530

    bugfix

--- documents/models.py
+++ documents/models.py
@@ -25,6 +25,7 @@ class Document(models.Model):
             if self.custom_user.upload_till_now > settings.MAX_UPLOAD_PER_USER:
                 raise PermissionDenied("Maximum upload limit exceeded")
             self.custom_user.upload_till_now += self.document.size
+            self.custom_user.save()
 
         import hashlib
         self.sha_256 = hashlib.sha256(self.document.read()).hexdigest()
