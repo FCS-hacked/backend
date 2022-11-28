f2e71fef3b6d4320301ac67e0472bfb9394ef257
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-11-28 21:12:26 +0530

    bugfix

--- unauth/password_validator.py
+++ unauth/password_validator.py
@@ -5,9 +5,9 @@ from django.utils.translation import gettext as _
 class UnauthPasswordValidator:
     def validate(self, password: str, user):
         if password.lower() == password or password.upper() == password or \
-                len({'!', '$', '%', '*', '(', '#', ')', '@', '^', '&'}.intersection(password)) == 0:
+                len({'!', '$', '%', '*', '(', '-', '#', ')', '@', '^', '&'}.intersection(password)) == 0:
             raise ValidationError(
-                _("Password must contain at least one uppercase and lowercase letter and one of !@#$%^&*()."),
+                _("Password must contain at least one uppercase and lowercase letter and one of !@#$%^&*(-)."),
                 code="password_no_upper_lower_special_character",
             )
 
